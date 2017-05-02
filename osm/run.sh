cd6d7139a0e36f16f1e6db2433f6fdc0f2850433
Peter France <peter.france@moat.com>
2017-05-02 11:44:39 -0600

    docker stuff works

--- osm/run.sh
+++ osm/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-DDIR=$(mktemp -d)
-curl $READURL > $DDIR/proto.pbf
-imposm3 import  -mapping $MAPPING -read $DDIR/proto.pbf -write -optimize -connection "postgis://trailio:trailio@$HOSTIP:5432/trailio" -overwritecache
+$GOPATH/bin/imposm3 import  -mapping $MAPPING -read /root/data/$FILENAME -write -optimize -connection "postgis://trailio:trailio@$HOSTIP:5432/trailio" -overwritecache
+
--- osm/scripts/runImposm.sh
+++ osm/scripts/runImposm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-READURL=
-MAPPING=
+#READURL=
+#MAPPING=
 
 while getopts "r:m:" opt; do
   case $opt in
@@ -22,7 +22,20 @@ done
 
 
 set -u
-HOSTIP=$(ipconfig getifaddr en0)
-#HOSTIP=$(docker-machine ip)
+ 
+HOSTIP="10.200.10.1"
+sudo ifconfig lo0 alias $HOSTIP
+echo $HOSTIP
 
-docker run -p 5432:5432 -e "HOSTIP=$HOSTIP" -e "READURL=$READURL" -e "MAPPING=$MAPPING" --net=host -t -i ewestern/osm
+DATADIR=$(pwd)/data/
+filename=$(basename $READURL)
+if [ ! -f $DATADIR/$filename ]; then
+  wget -nc $READURL -P $DATADIR
+fi
+CONTDATA=/root/data/
+
+LOCALHOST="0.0.0.0"
+#docker run -p 5432:5432 -v "$DATADIR:$CONTDATA" -e "HOSTIP=$LOCALHOST" -e "FILENAME=$filename" -e "MAPPING=$MAPPING" --net=host -t -i ewestern/osm  bin/run.sh
+docker run -v "$DATADIR:$CONTDATA" -e "HOSTIP=$HOSTIP" -e "FILENAME=$filename" -e "MAPPING=$MAPPING" -t -i --net=host ewestern/osm bin/run.sh
+
+docker ps --all | awk '{ print $1 }' | tail -n +2  | xargs docker rm
